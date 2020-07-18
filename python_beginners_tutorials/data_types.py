45ac4d0f83f8c33918519a6c5002c42638608bf3
Rashid Mahmood <rashiidmahmood@gmail.com>
2020-07-18 14:46:08 +0400

    hello_world and data_types

---
+++ python_beginners_tutorials/data_types.py
@@ -0,0 +1,36 @@
+x = 1
+print(x)
+
+# int, float and string
+number = 7
+print(type(number))
+fl_number = 4.5
+print(type(fl_number))
+str_val = "This is just a sample string"
+print(type(str_val))
+
+
+# operations
+num1 = 10
+num2 = 25
+num1 = 5.7
+num1 = 5
+num3 = "15"
+print(type(int(num3)))
+print(num1+int(num3))
+
+
+num1 = input("Enter a number here:")
+num1 = int(num1)
+print(num1)
+num2 = 10
+print(num1+num2)
+
+str1 = "My name is "
+str2 = "Rashid"
+print(str1 + str2)
+
+# Write a program which will take 2 inputs from the user
+# and will perform sum operation between these 2 numbers
+# let say 1st input is 5 and 2nd input is 10, then the output
+# should be 5+10 = 15
