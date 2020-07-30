5c045353466581ab026c09c42f2d8f347d3c0905
Rashid Mahmood <rashiidmahmood@gmail.com>
2020-07-30 21:44:54 +0400

    dictionary.py

--- python_beginners_tutorials/dictionary.py
+++ python_beginners_tutorials/dictionary.py
@@ -18,3 +18,37 @@ print(person.keys())
 print(person.values())
 del person['gender']
 print(person)
+
+
+car_dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+# Changing value of a specific item
+car_dict['year'] = 2000
+print(car_dict)
+
+# Check if key exists in the dictionary
+print('model' in car_dict)
+
+# Adding item to a dictionary
+car_dict['color'] = 'Black'
+print(car_dict)
+
+# Adding dictionary to a dictionary
+car_dict2 = {
+    'width': '75.4 in',
+    'height': '54.3 in'
+}
+car_dict.update(car_dict2)
+print(car_dict)
+
+# Removing a particular item from the dictionary
+print(car_dict.pop('year2', 'Not Found'))
+print(car_dict)
+
+# Remove last inserted item
+car_dict.popitem()
+print(car_dict)
