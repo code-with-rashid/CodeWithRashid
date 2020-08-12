362d342f20f54c1e8d19abbf96b6022ec31c5aeb
Rashid Mahmood <rashiidmahmood@gmail.com>
2020-08-12 23:27:39 +0400

    - arithmetic operators

---
+++ python_beginners_tutorials/operators.py
@@ -0,0 +1,44 @@
+# Python Operators
+# 1- Arithmetic operators
+# 2- Assignment operators
+# 3- Comparison operators
+# 4- Logical operators
+# 5- Identity operators
+# 6- Membership operators
+# 7- Bitwise operators
+
+
+
+
+
+# 1- Arithmetic operators
+a = 12
+b = 5
+
+# Addition
+print(f'{a} + {b}: ', a + b)
+
+# Subtraction
+print(f'{a} - {b}: ', a - b)
+
+# Multiplication
+print(f'{a} * {b}: ', a * b)
+
+# Division
+print(f'{a} / {b}: ', a / b)
+
+# Modulus
+# a = 15
+# b = 5
+print(f'{a} % {b}: ', a % b)
+
+# Exponentiation
+# a = 5
+# b = 2
+print(f'{a} ** {b}: ', a ** b)
+
+# Floor division
+a = 13
+b = 5
+print(f'{a} / {b}: ', a / b)
+print(f'{a} // {b}: ', a // b)
