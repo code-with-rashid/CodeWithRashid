e9e4363e8dcc8da0fcc4482dfbd18bc34fe6f245
Rashid Mahmood <rashiidmahmood@gmail.com>
2020-08-22 22:19:21 +0400

    - logical_Identity_and_membership_operators

---
+++ python_beginners_tutorials/logical_Identity_and_membership_operators.py
@@ -0,0 +1,23 @@
+# Python Logical Operators
+a = 10
+b = 12
+c = 15
+# and
+# print(a<b and b<c)
+# or
+# print(a>b or b>c)
+# not
+# print(not a>b)
+
+# Python Identity Operators
+a = 10
+b = 15
+# print(id(a))
+# print(id(b))
+# print(a is b)
+# print(a is not b)
+
+# Python Membership Operators
+arr = [1,2,3,4,5,6,7,8,9]
+print(1 in arr)
+print(10 not in arr)
