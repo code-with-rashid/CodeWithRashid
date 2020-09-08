4430b8244845f0946729a93daa87df4b4c7ef07c
Rashid Mahmood <rashiidmahmood@gmail.com>
2020-09-08 23:47:20 +0400

    - while loop

---
+++ python_beginners_tutorials/if_elif_else.py
@@ -0,0 +1,23 @@
+# Conditional Statements in Python
+# if
+# elif
+# else
+# nested if
+
+a = 29
+b = 20
+
+if a>b:
+    pass #we'll add this logic later
+
+# if a>b:
+#     print("a is greater than b")
+#     if a >30:
+#         print("a is also greater than 30")
+
+# elif a==b:
+#     print("a is equal to b")
+# else:
+#     print("a is less than b")
+
+# print("this is just a test statment")
---
+++ python_beginners_tutorials/while_loop.py
@@ -0,0 +1,11 @@
+# 1- While loop
+# 2- For loop
+i = 1
+while i<4:
+    i = i+1
+    if i == 2:
+        continue
+        # break
+    print(i)
+else:
+    print("While loop has ended")
